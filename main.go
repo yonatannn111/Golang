52fe6fa8dfd7c6768746f0839c9c29d41caef320
yonatannn111 <yonitiman@gmail.com>
2025-08-23 20:25:04 +0300

    creating a database connection

--- main.go
+++ main.go
@@ -5,15 +5,34 @@ import (
 	"log"
 	"os"
 	"github.com/joho/godotenv"
+	_"github.com/lib/pq"
 
 )
+type apiConfig struct {
+	DB *database.queries
+}
 func main() {
 	fmt.Println("Hello, World! ")
 	godotenv.Load(".env")
+	
 	portString:= os.Getenv("PORT") 
 	if portString ==""{
 		log.Fatal("PORT is not found in the environment")
 	}
+
+
+	dbURL:= os.Getenv("DB_URL") 
+	if dbURL ==""{
+		log.Fatal("DB_URL is not found in the environment")
+	}
+conn, err := sql.Open("postgres", dbURL)
+if err!=nil{
+	log.Fatal("Cannot connect to database:", err)
+}
+
+apicfg:= apiConfig{
+DB: database.New(conn),
+}
 	fmt.Println("Port is set to:", portString)
 }
 
